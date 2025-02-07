5925413838be3dd5ea6399a171865220f1ef295e
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-02-07 19:51:30 +0300

    Initialize Spring Boot project with Maven wrapper and basic structure

---
+++ src/main/java/com/ecommerce/project/SebEcomPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebEcomPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SebEcomPracticeApplication.class, args);
+    }
+
+}
