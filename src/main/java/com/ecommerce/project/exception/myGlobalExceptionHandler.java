f0a13746e9b788b951601ba0a5fb84b42dd29af8
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-02-21 15:10:18 +0300

    Handle Custom Exception
    Implemented a global exception handling strategy, including myGlobalExceptionHandler, ResourceNotFoundException, APIException

---
+++ src/main/java/com/ecommerce/project/exception/myGlobalExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.ecommerce.project.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class myGlobalExceptionHandler {
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> myMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        
+        ex.getBindingResult().getAllErrors().forEach((err) -> {
+            String fieldName = ((FieldError) err).getField();
+            String errorMessage = err.getDefaultMessage();
+            
+            errors.put(fieldName, errorMessage);
+        });
+        
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+    
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> myResourceNotFoundException(ResourceNotFoundException ex) {
+        String message = ex.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    }
+    
+    @ExceptionHandler(APIException.class)
+    public ResponseEntity<String> myAPIException(APIException ex) {
+        String message = ex.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+}
