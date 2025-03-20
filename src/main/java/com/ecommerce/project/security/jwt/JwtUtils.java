e3e21866cefe3bd1dc2f0f606b1f90220860e3d6
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-03-20 19:38:22 +0300

    Add functionality to verify jwt tokens

--- src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
+++ src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
@@ -1,14 +1,14 @@
 package com.ecommerce.project.security.jwt;
 
 import com.ecommerce.project.security.services.UserDetailsImpl;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-
 import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
@@ -25,7 +25,7 @@ public class JwtUtils {
     public String getJwtFromHeader(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
         
-        if(bearerToken != null && bearerToken.startsWith("Bearer ")) {
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7);
         }
         
@@ -52,7 +52,27 @@ public class JwtUtils {
             .getSubject();
     }
     
-    
+    public boolean validateJwtToken(String authToken) {
+        try {
+            System.out.println("Validating");
+            Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build()
+                .parseSignedClaims(authToken);
+            
+            return true;
+        } catch (MalformedJwtException ex) {
+            logger.error("Invalid JWT token: {}", ex.getMessage());
+        } catch (ExpiredJwtException ex) {
+            logger.error("Expired JWT token: {}", ex.getMessage());
+        } catch (UnsupportedJwtException ex) {
+            logger.error("JWT token is unsupported: {}", ex.getMessage());
+        } catch (IllegalArgumentException ex) {
+            logger.error("JWT claims string is empty: {}", ex.getMessage());
+        }
+        
+        return false;
+    }
     
     private Key key() {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
