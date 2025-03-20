7c205a68cf9c42c70b6f3769387489db654d7c11
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-03-20 19:23:39 +0300

    Add functionality to generate a token a username, as well as getting  the username from a token

--- src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
+++ src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
@@ -1,10 +1,17 @@
 package com.ecommerce.project.security.jwt;
 
+import com.ecommerce.project.security.services.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
 
 public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
@@ -25,7 +32,29 @@ public class JwtUtils {
         return null;
     }
     
-   /* public String generateTokenFromUserName(UserDetails userDetails) {
+    public String generateTokenFromUserName(UserDetailsImpl userDetails) {
+        String username = userDetails.getUsername();
+        
+        return Jwts.builder()
+            .subject(username)
+            .issuedAt(new Date())
+            .expiration(new Date((new Date()).getTime() + jwtExpirationMs))
+            .signWith(key())
+            .compact();
+    }
     
-    }*/
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser()
+            .verifyWith((SecretKey) key())
+            .build()
+            .parseSignedClaims(token)
+            .getPayload()
+            .getSubject();
+    }
+    
+    
+    
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
 }
