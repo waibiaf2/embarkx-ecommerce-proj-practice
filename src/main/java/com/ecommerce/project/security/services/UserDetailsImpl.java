9c5a84b7d631625d35c574607767f1526b422964
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-03-26 22:28:33 +0300

    Refactor UserRepository and UserDetailsService: update method names for consistency and improve code readability

--- src/main/java/com/ecommerce/project/security/services/UserDetailsImpl.java
+++ src/main/java/com/ecommerce/project/security/services/UserDetailsImpl.java
@@ -87,10 +87,10 @@ public class UserDetailsImpl implements UserDetails {
     
     @Override
     public boolean equals(Object o) {
-        if (this == o)
+        if(this == o)
             return true;
         
-        if (o == null || getClass() != o.getClass())
+        if(o==null || getClass() != o.getClass())
             return false;
         
         UserDetailsImpl user = (UserDetailsImpl) o;
--- src/main/java/com/ecommerce/project/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/ecommerce/project/security/services/UserDetailsServiceImpl.java
@@ -2,7 +2,6 @@ package com.ecommerce.project.security.services;
 
 import com.ecommerce.project.models.User;
 import com.ecommerce.project.repositories.UserRepository;
-import org.hibernate.annotations.SecondaryRow;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,7 +17,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username).orElseThrow(
+        User user = userRepository.findByUserName(username).orElseThrow(
             () -> new UsernameNotFoundException("User not found with username: " + username)
         );
         return UserDetailsImpl.build(user);
