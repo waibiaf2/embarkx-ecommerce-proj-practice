87f1894758f674f8e4e6516d882b3ec7283a4e44
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-03-26 22:27:40 +0300

    Add MessageResponse and UserInfoResponse DTOs with Lombok annotations for simplified data handling

--- src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
+++ src/main/java/com/ecommerce/project/security/response/UserInfoResponse.java
@@ -1,4 +1,35 @@
 package com.ecommerce.project.security.response;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class UserInfoResponse {
+    private Long id;
+    private String jwtToken;
+    private String username;
+    private List<String> roles;
+    
+    public UserInfoResponse(
+        Long id,
+        String jwtToken,
+        String username,
+        List<String> roles
+    ) {
+        this.id = id;
+        this.jwtToken = jwtToken;
+        this.username = username;
+        this.roles = roles;
+    }
+    
+    public UserInfoResponse(
+        Long id,
+        String username,
+        List<String> roles
+    ) {
+        this.id = id;
+        this.username = username;
+        this.roles = roles;
+    }
 }
